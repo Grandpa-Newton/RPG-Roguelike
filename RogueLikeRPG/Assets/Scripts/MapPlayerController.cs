4d7fa230e43c35ec25b67781c3b2cc0095c6cbd5
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-01-29 11:26:15 +0300

    Refactoring

--- RogueLikeRPG/Assets/Scripts/MapPlayerController.cs
+++ RogueLikeRPG/Assets/Scripts/MapPlayerController.cs
@@ -11,7 +11,7 @@ public class MapPlayerController : MonoBehaviour
     public event Action OnCurrentCell; // когда пользователь встал на новую клетку (может быть, поменять название)
 
     public event Action OnInteractCell;
-    
+
     [SerializeField] private float _speed;
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] LayerMask _layerMask;
@@ -33,7 +33,7 @@ public class MapPlayerController : MonoBehaviour
         Instance = this;
 
         _rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
     private void Start()
@@ -52,60 +52,54 @@ public class MapPlayerController : MonoBehaviour
 
             raycastHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, rayDistance, _layerMask);
 
-            if (raycastHit)
+            if (raycastHit && raycastHit.transform != null)
             {
-                if(raycastHit.transform != null)
+                Debug.Log(raycastHit.transform.gameObject);
+                Transform interactingCellTransform = raycastHit.transform;
+                if (interactingCellTransform.gameObject.TryGetComponent(out _interactingCell) && _interactingCell.CellType == CellType.Active)
                 {
-                    Debug.Log(raycastHit.transform.gameObject);
-                    Transform interactingCellTransform = raycastHit.transform;
-                    if (interactingCellTransform.gameObject.TryGetComponent(out _interactingCell))
+                    BaseCell[] cells = UnityEngine.Object.FindObjectsOfType<BaseCell>(); // мб как-то поменять / вынести в отдельный метод
+
+                    BaseCell currentCell = FindCurrentCell(cells); // ПРОВЕРКА НА != NULL
+
+                    if (currentCell != null)
                     {
-                        if (_interactingCell.CellType == CellType.Active)
+                        foreach (GameObject neighbor in currentCell.NeighborsCells)
                         {
-                            BaseCell[] cells = UnityEngine.Object.FindObjectsOfType<BaseCell>(); // мб как-то поменять / вынести в отдельный метод
-
-                            BaseCell currentCell = FindCurrentCell(cells); // ПРОВЕРКА НА != NULL
-
-                            if(currentCell != null)
-                            {
-                                foreach (GameObject neighbor in currentCell.NeighborsCells)
-                                {
-                                    neighbor.GetComponent<BaseCell>().CellType = CellType.Inactive;
-                                }
-
-                                currentCell.CellType = CellType.Inactive;
-
-                                MapLoader.CurrentCellId = interactingCellTransform.GetComponent<BaseCell>().CellId;
-
-
-                                // УБРАТЬ ЦИКЛ НИЖЕ, ЭТО ДЛЯ ТЕСТА!
-
-                                /*foreach (GameObject neighbor in interactingCellTransform.GetComponent<NormalCell>().NeighborsCells)
-                                {
-                                    neighbor.GetComponent<NormalCell>().IsActive = true;
-                                }*/
-
-                                _clickedCellTransform = raycastHit.transform;
-
-                                _isMoving = true;
-
-                                _interactingCell.CellType = CellType.Active;
-
-                            }
-                            else
-                            {
-                                Debug.Log("BaseCell Component is not attached to object");
-                            }
+                            neighbor.GetComponent<BaseCell>().CellType = CellType.Inactive;
                         }
-                        else
+
+                        currentCell.CellType = CellType.Inactive;
+
+                        MapLoader.CurrentCellId = interactingCellTransform.GetComponent<BaseCell>().CellId;
+
+                        // ЦИКЛ НИЖЕ - ДЛЯ ТЕСТА!
+
+                        /*foreach (GameObject neighbor in interactingCellTransform.GetComponent<NormalCell>().NeighborsCells)
                         {
-                            Debug.Log("The cell is not active");
-                        }
+                            neighbor.GetComponent<NormalCell>().IsActive = true;
+                        }*/
+
+                        _clickedCellTransform = raycastHit.transform;
+
+                        _isMoving = true;
+
+                        _interactingCell.CellType = CellType.Active; // может быть, поменять
+
+                    }
+                    else
+                    {
+                        Debug.Log("BaseCell Component is not attached to object");
                     }
                 }
+                else
+                {
+                    Debug.Log("The cell is not active");
+                }
+
             }
         }
-        
+
         if (_isMoving)
         {
             if ((Vector2)transform.position != (Vector2)_clickedCellTransform.position)
