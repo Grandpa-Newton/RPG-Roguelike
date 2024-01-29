a790808e5c6f4a240104d08911914c4d46f64323
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-01-29 22:54:07 +0300

    Доделал улучшенную генерацию
    Осталось только спавн врагов и предметов

--- RogueLikeRPG/Assets/Scripts/NewScripts/TilemapVisualizer.cs
+++ RogueLikeRPG/Assets/Scripts/NewScripts/TilemapVisualizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,8 +12,15 @@ public class TilemapVisualizer : MonoBehaviour
 
     [SerializeField] private TileBase floorTile;
 
-    [SerializeField] private TileBase wallTop;
+    [SerializeField] private TileBase wallTop, wallSideRight, wallSideLeft, wallBottom, wallFull;
 
+    [SerializeField] private TileBase
+        wallInnerCornerDownLeft,
+        wallInnerCornerDownRight,
+        wallDiagonalCornerDownLeft,
+        wallDiagonalCornerDownRight,
+        wallDiagonalCornerUpLeft,
+        wallDiagonalCornerUpRight;
     public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
     {
         PaintTiles(floorPositions, floorTilemap, floorTile);
@@ -38,8 +46,73 @@ public class TilemapVisualizer : MonoBehaviour
         wallTilemap.ClearAllTiles();
     }
 
-    public void PaintSingleBasicWall(Vector2Int position)
+    public void PaintSingleBasicWall(Vector2Int position, string binaryType)
     {
-        PaintSingleTile(wallTilemap,wallTop,position);
+        int typeAsInt = Convert.ToInt32(binaryType, 2);
+        TileBase tile = null;
+        if (WallTypesHelper.wallTop.Contains(typeAsInt))
+        {
+            tile = wallTop;
+        }
+        else if (WallTypesHelper.wallBottom.Contains(typeAsInt))
+        {
+            tile = wallBottom;
+        }
+        else if (WallTypesHelper.wallSideLeft.Contains(typeAsInt))
+        {
+            tile = wallSideLeft;
+        }
+        else if (WallTypesHelper.wallSideRight.Contains(typeAsInt))
+        {
+            tile = wallSideRight;
+        }
+        else if (WallTypesHelper.wallFull.Contains(typeAsInt))
+        {
+            tile = wallFull;
+        }
+
+        if (tile != null)
+            PaintSingleTile(wallTilemap, tile, position);
+    }
+
+    public void PaintSingleCornerWall(Vector2Int position, string binaryType)
+    {
+        int typeAsInt = Convert.ToInt32(binaryType, 2);
+        TileBase tile = null;
+
+        if (WallTypesHelper.wallInnerCornerDownLeft.Contains(typeAsInt))
+        {
+            tile = wallInnerCornerDownLeft;
+        }
+        else if (WallTypesHelper.wallInnerCornerDownRight.Contains(typeAsInt))
+        {
+            tile = wallInnerCornerDownRight;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerDownLeft.Contains(typeAsInt))
+        {
+            tile = wallDiagonalCornerDownLeft;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerDownRight.Contains(typeAsInt))
+        {
+            tile = wallDiagonalCornerDownRight;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerUpLeft.Contains(typeAsInt))
+        {
+            tile = wallDiagonalCornerUpLeft;
+        }
+        else if (WallTypesHelper.wallDiagonalCornerUpRight.Contains(typeAsInt))
+        {
+            tile = wallDiagonalCornerUpRight;
+        }
+        else if (WallTypesHelper.wallFullEightDirections.Contains(typeAsInt))
+        {
+            tile = wallFull;
+        }
+        else if (WallTypesHelper.wallBottmEightDirections.Contains(typeAsInt))
+        {
+            tile = wallBottom;
+        }
+        if(tile != null)
+            PaintSingleTile(wallTilemap, tile, position);
     }
 }
