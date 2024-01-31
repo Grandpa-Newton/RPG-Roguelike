7602a6b0afb0e29b28d3416e81252397f36d6420
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-01-31 16:03:34 +0300

    New Input System on Map
    Нужно проверить, правильно ли работает, но, вроде бы, переписал под новую систему ввода (теперь не мышкой, а кнопками выбирается уровень)

--- RogueLikeRPG/Assets/Scripts/Player/MapPlayerController.cs
+++ RogueLikeRPG/Assets/Scripts/Player/MapPlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapPlayerController : MonoBehaviour
 {
@@ -28,7 +29,8 @@ public class MapPlayerController : MonoBehaviour
 
     private bool _isSelecting = false; // производит ли сейчас выбор клетки пользователь
 
-    private GameObject _selectingCell = null;
+    [HideInInspector]
+    public GameObject SelectingCell = null;
 
     private List<GameObject> _activeCells = new List<GameObject>();
 
@@ -69,11 +71,16 @@ public class MapPlayerController : MonoBehaviour
     private void GetCurrentCells()
     {
         _activeCells = MapLoader.Instance.ActiveCells.OrderBy(c => c.transform.localPosition.x).ToList(); // гениальная разработка, но нужно менять!
-        _selectingCell = _activeCells[0];
-        if(_selectingCell != null)
+        foreach(var cell in _activeCells)
         {
-            _selectingCell.GetComponent<BaseCell>().CellType = CellType.Selecting;
-            _playerInputActions.Map.SelectCell.performed += SelectCell_performed;
+            cell.GetComponent<Selectable>().enabled = true;
+        }
+        SelectingCell = _activeCells[0];
+        if(SelectingCell != null)
+        {
+            SelectingCell.GetComponent<Selectable>().Select();
+            // _selectingCell.GetComponent<BaseCell>().CellType = CellType.Selecting;
+            _playerInputActions.Map.ConfirmCell.performed += ConfirmCell_performed;
         }
         else
         {
@@ -81,9 +88,9 @@ public class MapPlayerController : MonoBehaviour
         }
     }
 
-    private void SelectCell_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void ConfirmCell_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-
+        SelectCell();
     }
 
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -98,14 +105,33 @@ public class MapPlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !_isMoving) // ПОМЕНЯТЬ
+        if (_isMoving)
+        {
+            if ((Vector2)transform.position != (Vector2)_clickedCellTransform.position)
+            {
+                _moveDirection = Vector2.MoveTowards(transform.position, (Vector2)_clickedCellTransform.position, _speed * Time.deltaTime);
+                transform.position = _moveDirection;
+            }
+            else
+            {
+                _clickedCellTransform.GetComponent<StartNextLevel>().InCurrentCell();
+                _interactingCell.CellType = CellType.Current;
+                // _interactingCell.Interact();
+                _isMoving = false;
+            }
+        }
+    }
+
+    private void SelectCell()
+    {
+        /* if (!_isMoving) // ПОМЕНЯТЬ
         {
             Debug.Log("In Input");
             RaycastHit2D raycastHit;
 
             float rayDistance = 100.0f;
 
-            raycastHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, rayDistance, _layerMask);
+            raycastHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, rayDistance, _layerMask); 
 
             if (raycastHit && raycastHit.transform != null)
             {
@@ -133,7 +159,7 @@ public class MapPlayerController : MonoBehaviour
                         /*foreach (GameObject neighbor in interactingCellTransform.GetComponent<NormalCell>().NeighborsCells)
                         {
                             neighbor.GetComponent<NormalCell>().IsActive = true;
-                        }*/
+                        }
 
                         _clickedCellTransform = raycastHit.transform;
 
@@ -153,23 +179,65 @@ public class MapPlayerController : MonoBehaviour
                 }
 
             }
+        }*/
+        if (!_isMoving) // ПОМЕНЯТЬ
+        {
+            Debug.Log("In Input");
+           
+            if (SelectingCell != null)
+            {
+                // Debug.Log(raycastHit.transform.gameObject);
+                // Transform interactingCellTransform = raycastHit.transform;
+                if (SelectingCell.gameObject.TryGetComponent(out _interactingCell) && _interactingCell.CellType == CellType.Selecting)
+                {
+                    BaseCell[] cells = UnityEngine.Object.FindObjectsOfType<BaseCell>(); // мб как-то поменять / вынести в отдельный метод
+
+                    BaseCell currentCell = FindCurrentCell(cells);
+
+                    if (currentCell != null)
+                    {
+                        foreach (GameObject neighbor in currentCell.NeighborsCells)
+                        {
+                            neighbor.GetComponent<BaseCell>().CellType = CellType.Inactive;
+                        }
+
+                        currentCell.CellType = CellType.Inactive;
+
+                        MapLoader.CurrentCellId = _interactingCell.CellId;
+
+                        // ЦИКЛ НИЖЕ - ДЛЯ ТЕСТА!
+
+                        /*foreach (GameObject neighbor in interactingCellTransform.GetComponent<NormalCell>().NeighborsCells)
+                        {
+                            neighbor.GetComponent<NormalCell>().IsActive = true;
+                        }*/
+
+                        _clickedCellTransform = SelectingCell.transform;
+
+                        _playerInputActions.Map.Interact.performed += Interact_performed;
+
+                        _playerInputActions.Map.ConfirmCell.performed -= ConfirmCell_performed;
+
+                        _isMoving = true;
+
+                        _interactingCell.CellType = CellType.Active; // может быть, поменять
+
+
+
+                    }
+                    else
+                    {
+                        Debug.LogError("BaseCell Component is not attached to object");
+                    }
+                }
+                else
+                {
+                    Debug.Log("The cell is not active");
+                }
+
+            }
         }
 
-        if (_isMoving)
-        {
-            if ((Vector2)transform.position != (Vector2)_clickedCellTransform.position)
-            {
-                _moveDirection = Vector2.MoveTowards(transform.position, (Vector2)_clickedCellTransform.position, _speed * Time.deltaTime);
-                transform.position = _moveDirection;
-            }
-            else
-            {
-                _clickedCellTransform.GetComponent<StartNextLevel>().InCurrentCell();
-                _interactingCell.CellType = CellType.Current;
-                // _interactingCell.Interact();
-                _isMoving = false;
-            }
-        }
     }
 
     private BaseCell FindCurrentCell(BaseCell[] cells)
--- RogueLikeRPG/Assets/Scripts/TestClasses/TestSelectHandler.cs
+++ RogueLikeRPG/Assets/Scripts/TestClasses/TestSelectHandler.cs
@@ -15,6 +15,7 @@ public class TestSelectHandler : MonoBehaviour, ISelectHandler, IDeselectHandler
     public void OnSelect(BaseEventData eventData)
     {
         gameObject.GetComponent<BaseCell>().CellType = DefaultNamespace.CellType.Selecting;
+        MapPlayerController.Instance.SelectingCell = gameObject;
         BaseCell currentCell = gameObject.GetComponent<BaseCell>();
         _previousType = currentCell.CellType;
         currentCell.CellType = DefaultNamespace.CellType.Selecting;
