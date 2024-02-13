e1ce51e4d537c0ac43e2342e667635fb9c41f776
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-02-13 19:07:37 +0300

    Pick up items in inventory

--- RogueLikeRPG/Assets/Scripts/ScriptableObjectsScripts/Model/InventorySO.cs
+++ RogueLikeRPG/Assets/Scripts/ScriptableObjectsScripts/Model/InventorySO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 using Unity.VisualScripting;
 
 namespace Inventory.Model
@@ -11,9 +12,9 @@ namespace Inventory.Model
     {
         [SerializeField] private List<InventoryItem> inventoryItems;
 
-        public event Action<Dictionary<int, InventoryItem>> OnInventoryUpdated; 
+        public event Action<Dictionary<int, InventoryItem>> OnInventoryUpdated;
         [field: SerializeField] public int Size { get; private set; } = 10;
-        
+
         public void Initialize()
         {
             inventoryItems = new List<InventoryItem>();
@@ -23,26 +24,95 @@ namespace Inventory.Model
             }
         }
 
-        private void AddItem(ItemSO item, int quantity)
+        public int AddItem(ItemSO item, int quantity)
+        {
+            if (item.IsStackable == false)
+            {
+                for (int i = 0; i < inventoryItems.Count; i++)
+                {
+                    if (IsInventoryFull())
+                        return quantity;
+                    while (quantity > 0 && !IsInventoryFull())
+                    {
+                       quantity -= AddItemToFirstFreeSlot(item, 1);
+                    }
+                    InformAboutChanges();
+                    return quantity;
+                }
+            }
+
+            quantity = AddStackableItem(item, quantity);
+            InformAboutChanges();
+            return quantity;
+        }
+
+        private int AddItemToFirstFreeSlot(ItemSO item, int quantity)
+        {
+            InventoryItem newItem = new InventoryItem()
+            {
+                item = item,
+                quantity = quantity
+            };
+
+            for (int i = 0; i < inventoryItems.Count; i++)
+            {
+                if (inventoryItems[i].IsEmpty)
+                {
+                    inventoryItems[i] = newItem;
+                    return quantity;
+                }
+            }
+
+            return 0;
+        }
+
+        private bool IsInventoryFull()
+        {
+            return inventoryItems.Where(item => item.IsEmpty).Any() == false;
+        }
+
+        private int AddStackableItem(ItemSO item, int quantity)
         {
             for (int i = 0; i < inventoryItems.Count; i++)
             {
                 if (inventoryItems[i].IsEmpty)
                 {
-                    inventoryItems[i] = new InventoryItem
+                    continue;
+                }
+
+                if (inventoryItems[i].item.ID == item.ID)
+                {
+                    int amountPossibleToTake = inventoryItems[i].item.MaxStackSize - inventoryItems[i].quantity;
+
+                    if (quantity > amountPossibleToTake)
                     {
-                        item = item,
-                        quantity = quantity,
-                    };
-                    return;
+                        inventoryItems[i] = inventoryItems[i].ChangeQuantity(inventoryItems[i].item.MaxStackSize);
+                        quantity -= amountPossibleToTake;
+                    }
+                    else
+                    {
+                        inventoryItems[i] = inventoryItems[i].ChangeQuantity(inventoryItems[i].quantity + quantity);
+                        InformAboutChanges();
+                        return 0;
+                    }
                 }
             }
+
+            while (quantity > 0 && !IsInventoryFull())
+            {
+                int newQuantity = Mathf.Clamp(quantity, 0, item.MaxStackSize);
+                quantity -= newQuantity;
+                AddItemToFirstFreeSlot(item, newQuantity);
+            }
+
+            return quantity;
         }
 
         public void AddItem(InventoryItem item)
         {
             AddItem(item.item, item.quantity);
         }
+
         public Dictionary<int, InventoryItem> GetCurrentInventoryState()
         {
             Dictionary<int, InventoryItem> returnValue = new Dictionary<int, InventoryItem>();
@@ -63,7 +133,8 @@ namespace Inventory.Model
 
         public void SwapItems(int itemIndex1, int itemIndex2)
         {
-            (inventoryItems[itemIndex1], inventoryItems[itemIndex2]) = (inventoryItems[itemIndex2], inventoryItems[itemIndex1]);
+            (inventoryItems[itemIndex1], inventoryItems[itemIndex2]) =
+                (inventoryItems[itemIndex2], inventoryItems[itemIndex1]);
             InformAboutChanges();
         }
 
