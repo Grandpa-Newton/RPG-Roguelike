c4b835b6f4865fd78185b9e018cae4b24b5eb289
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-03-04 14:42:00 +0300

    Adding Random Generation of Cell Types

--- RogueLikeRPG/Assets/Scripts/Map/MapLoader.cs
+++ RogueLikeRPG/Assets/Scripts/Map/MapLoader.cs
@@ -1,15 +1,28 @@
 using DefaultNamespace;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class MapLoader : MonoBehaviour
 {
     [HideInInspector]
     public List<GameObject> ActiveCells = new List<GameObject>();
 
+
+    [SerializeField]
+    private List<SerializedDictionaryItem<CellSO, int>> CellTypesFrequencySerializedDictionary = new List<SerializedDictionaryItem<CellSO, int>>();
+
+    private Dictionary<CellSO, int> _cellTypesFrequencies = new Dictionary<CellSO, int>();
+
+    private static Dictionary<string, CellSO> _cellTypes = new Dictionary<string, CellSO>();
+    //private Dictionary<CellSO, int> CellTypesProbabilities = new Dictionary<CellSO, int>();
+
     public static string CurrentCellId;
+
+    private static bool WasSpawned = false;
 
     public static List<string> PassedCellsIds = new List<string>(); // список с пройденными клетками (по Id)
 
@@ -21,7 +34,8 @@ public class MapLoader : MonoBehaviour
     public static MapLoader Instance = null;
 
     public LoadTransition StartPanelLoadTransition;
-    private void Start()
+
+    private void Awake()
     {
         if (Instance == null)
         {
@@ -40,12 +54,109 @@ public class MapLoader : MonoBehaviour
 
         StartPanelLoadTransition.gameObject.SetActive(true);
 
+        if (!WasSpawned)
+        {
+            GenerateCellsType();
+        }
+    }
+    private void Start()
+    {
+        if (WasSpawned)
+        {
+            ApplyGeneratedTypes();
+        }
+
         UpdateInfo();
+    }
+
+    private void GenerateCellsType()
+    {
+
+        _cellTypesFrequencies = ConvertToDictinary<CellSO, int>(CellTypesFrequencySerializedDictionary);
+
+        // int currentFrequency = 0;
+
+        int sumOfFrequency = _cellTypesFrequencies.Sum(c => c.Value);
+
+        NormalCell[] cells = UnityEngine.Object.FindObjectsOfType<NormalCell>(); // все клетки без стартовой
+
+        foreach (var item in cells)
+        {
+
+            int currentFrequency = 0;
+
+
+            System.Random random = new System.Random();
+
+            int frequencyNumber = random.Next(0, sumOfFrequency + 1);
+
+            foreach (var cellTypeFrequency in _cellTypesFrequencies)
+            {
+                currentFrequency += cellTypeFrequency.Value;
+
+                if (frequencyNumber <= currentFrequency)
+                {
+                    _cellTypes[item.CellId] = cellTypeFrequency.Key;
+                    item.SetCellData(cellTypeFrequency.Key);
+                    break;
+                }
+
+            }
+        }
+
+        //_cellTypesProbabilities
+
+        /*int sumOfFrequency = cellTypesFrequencies.Sum(c => c.Value);
+
+        foreach (var cellTypeFrequency in cellTypesFrequencies)
+        {
+            _cellTypesProbabilities.Add(cellTypeFrequency.Key, (float)cellTypeFrequency.Value / (float)sumOfFrequency);
+        }*/
+
+
+
+
+        WasSpawned = true;
+    }
+
+    private void ApplyGeneratedTypes()
+    {
+
+
+        NormalCell[] cells = UnityEngine.Object.FindObjectsOfType<NormalCell>(); // все клетки без стартовой
+
+        foreach (var cell in cells)
+        {
+            cell.SetCellData(_cellTypes[cell.CellId]);
+        }
+
+        /*foreach (var item in cells)
+        {
+
+            int currentFrequency = 0;
+
+
+            System.Random random = new System.Random();
+
+            int frequencyNumber = random.Next(0, sumOfFrequency + 1);
+
+            foreach (var cellTypeFrequency in _cellTypesFrequencies)
+            {
+                currentFrequency += cellTypeFrequency.Value;
+
+                if (frequencyNumber <= currentFrequency)
+                {
+                    item.CellData = cellTypeFrequency.Key;
+                    break;
+                }
+
+            }
+        }*/
     }
 
     public void UpdateInfo()
     {
-        BaseCell[] cells = Object.FindObjectsOfType<BaseCell>();
+        BaseCell[] cells = UnityEngine.Object.FindObjectsOfType<BaseCell>();
 
         GameObject currentCell = null;
 
@@ -60,13 +171,13 @@ public class MapLoader : MonoBehaviour
             }
         }*/
 
-        if(currentCell != null)
+        if (currentCell != null)
         {
             BaseCell cell = currentCell.GetComponent<BaseCell>();
 
             PassedCellsIds.Add(cell.CellId);
 
-            foreach(var passedCell in PassedCellsIds)
+            foreach (var passedCell in PassedCellsIds)
             {
                 cells.First(c => c.CellId == passedCell).CellType = CellType.Passed;
             }
@@ -86,5 +197,25 @@ public class MapLoader : MonoBehaviour
 
             cell.CellType = CellType.Current;
         }
+    }
+
+    private Dictionary<T, K> ConvertToDictinary<T, K>(List<SerializedDictionaryItem<T, K>> serializedDictionary)
+    {
+        Dictionary<T, K> dictionary = new Dictionary<T, K>();
+        foreach (var item in serializedDictionary)
+        {
+            dictionary.Add(item.Key, item.Value);
+        }
+
+        return dictionary;
+    }
+
+
+
+    [Serializable]
+    public class SerializedDictionaryItem<T, K> // отдельный класс для того, чтобы сделать dictionary (словарь), который отображается в Inspector'е
+    {
+        public T Key;
+        public K Value;
     }
 }
