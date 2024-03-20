56f1dc84327e0de9fe1c0e281a9b39cf8659fa92
GrandpaNewton <ivan.gromyko.2804@gmail.com>
2024-03-20 11:32:16 +0300

    Trying to update inventory system
    Возникли проблемы, нужно будет обсудить сегодня

--- RogueLikeRPG/Assets/App/Scripts/DungeonScene/Items/TradableItemSO.cs
+++ RogueLikeRPG/Assets/App/Scripts/DungeonScene/Items/TradableItemSO.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Tradable_", menuName = "Trade")]
 public class TradableItemSO : ItemSO, IDestroyableItem, IItemAction
 {
+    public ItemSO ItemSO;
     public string ActionName => "Trade";
 
     public AudioClip itemActionSound { get; }
@@ -25,7 +26,7 @@ public class TradableItemSO : ItemSO, IDestroyableItem, IItemAction
         if (playerMoney.CanAffordReduceMoney(Cost)) // тут тоже, наверное, нужно количество
         {
             Debug.Log("Player can afford it");
-            if (character.GetComponent<TestTradingPlayerController>().TryAddItem(this)) // сюда нужно будет количество передавать
+            if (character.GetComponent<TestTradingPlayerController>().TryAddItem(ItemSO)) // сюда нужно будет количество передавать
             {
                 playerMoney.TryReduceMoney(Cost);
                 return true;
