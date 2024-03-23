1f1f90077d9b266bf608c7474437bcf17c92ed5b
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-03-23 20:27:52 +0300

    update generation for map_02 + sprite

--- RogueLikeRPG/Assets/App/Scripts/DungeonScene/GenerationsScripts/RandomTilemapGenerator.cs
+++ RogueLikeRPG/Assets/App/Scripts/DungeonScene/GenerationsScripts/RandomTilemapGenerator.cs
@@ -13,21 +13,7 @@ namespace App.Scripts.DungeonScene.GenerationsScripts
         [SerializeField] private int gridWidth;
         [SerializeField] private int gridHeight;
     
-        private void Start()
-        {
-            /* tilemap.ClearAllTiles();
-        for (int x = -gridWidth; x < gridWidth; x++)
-        {
-            for (int y = lowerBorder; y < gridHeight; y++)
-            {
-                Vector3Int position = new Vector3Int(x, y, 0);
-               
-                    tilemap.SetTile(position, tileBases[Random.Range(0, tileBases.Count)]);
-                
-               
-            }
-        } */
-        }
+
 
         /// <summary>
         /// 
